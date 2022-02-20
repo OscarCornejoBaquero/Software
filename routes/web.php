913dc8ea8db80dff4bacb7a3d540ae0eee32a7c3
Oscar-1989 <oscar.cornejob@ug.edu.ec>
2022-02-19 23:59:52 -0500

    Add Login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/','LoginController@login');
+Route::get('login','LoginController@login')->name('login');
